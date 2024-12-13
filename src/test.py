e78727ba1b2499cf8b3bcf65410cea7869be66f7
Killer66562 <jimmy688382@gmail.com>
2024-12-13 21:25:03 +0800

    Init

---
+++ src/test.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+
+import os
+
+
+TEST_TYPE = "eyes"
+
+MODELS_ROOT_DIR = "models"
+MODEL_FILENAME = "model-20241213-212054.pt"
+MODEL_PATH = os.path.join(MODELS_ROOT_DIR, TEST_TYPE, MODEL_FILENAME)
+
+TEST_IMAGES_ROOT_DIR = "test_images"
+
+TEST_IMAGE_OPEN_FILENAME = "open_0.jpeg"
+TEST_IMAGE_CLOSE_FILENAME = "close_0.jpeg"
+
+TEST_IMAGE_OPEN_PATH = os.path.join(TEST_IMAGES_ROOT_DIR, TEST_TYPE, TEST_IMAGE_OPEN_FILENAME)
+TEST_IMAGE_CLOSE_PATH = os.path.join(TEST_IMAGES_ROOT_DIR, TEST_TYPE, TEST_IMAGE_CLOSE_FILENAME)
+
+
+model = YOLO(MODEL_PATH)
+model.info(detailed=True)
+
+results = model.predict([TEST_IMAGE_CLOSE_PATH, TEST_IMAGE_OPEN_PATH])
+
+for result in results:
+    result.show()
---
+++ src/train.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+from datetime import datetime, timezone, timedelta
+
+import os
+
+
+WEIGHTS_ROOT_DIR = "weights"
+WEIGHT_FILENAME = "yolo11s-cls.pt"
+PRETRAINED_WEIGHT_PATH = os.path.join(WEIGHTS_ROOT_DIR, WEIGHT_FILENAME)
+
+TRAIN_TYPE = "eyes"
+
+DATASETS_ROOT_DIR = "datasets"
+DATASETS_PATH = os.path.join(DATASETS_ROOT_DIR, TRAIN_TYPE)
+
+IMG_SIZE = 96
+EPOCHS = 20
+BATCH_SIZE = 160
+OPTIMIZER = "sgd"
+LEARNING_RATE = 0.001
+
+MODELS_ROOT_DIR = "models"
+MODELS_EXPORT_DIR = os.path.join(MODELS_ROOT_DIR, TRAIN_TYPE)
+
+
+model = YOLO(model=PRETRAINED_WEIGHT_PATH, verbose=True)
+
+results = model.train(data=DATASETS_PATH, epochs=EPOCHS, batch=BATCH_SIZE, imgsz=IMG_SIZE, optimizer=OPTIMIZER, lr0=LEARNING_RATE)
+
+metrics = model.val(imgsz=IMG_SIZE)
+print(f"Best accuracy: {metrics.top1}")
+
+model_filename = "model-" + datetime.now(timezone(timedelta(hours=8))).strftime("%Y%m%d-%H%M%S") + ".pt"
+model_path = os.path.join(MODELS_EXPORT_DIR, model_filename)
+model.save(model_path)
